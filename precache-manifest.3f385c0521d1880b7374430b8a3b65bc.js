2c73b2146b81ea1b29e0f2dee0134550501ea414
Liz <divetus@gmail.com>
2020-10-28 15:12:44 -0400

    Updates

--- precache-manifest.3f385c0521d1880b7374430b8a3b65bc.js
+++ precache-manifest.3f385c0521d1880b7374430b8a3b65bc.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "412602d74f355592383c99ccce4ab02c",
+    "revision": "382b48d31132131ad1a8c2bb85b82718",
     "url": "/index.html"
   },
   {
-    "revision": "2021bcf0b0c46c06c244",
-    "url": "/static/css/main.1473fb4d.chunk.css"
+    "revision": "d96880f89cbc4717094e",
+    "url": "/static/css/main.dacd1a9c.chunk.css"
   },
   {
     "revision": "7409d92a3aba955b2b55",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.80364d31.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "2021bcf0b0c46c06c244",
-    "url": "/static/js/main.dcbb8770.chunk.js"
+    "revision": "d96880f89cbc4717094e",
+    "url": "/static/js/main.91b16ed5.chunk.js"
   },
   {
     "revision": "73eac8d40b3b607404fd",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e3671189ef7c786359c1a9c3ba1d5ad2.js"
+  "/precache-manifest.3f385c0521d1880b7374430b8a3b65bc.js"
 );
 
 self.addEventListener('message', (event) => {
